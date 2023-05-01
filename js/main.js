b715ec17d96c632e1f6e84160e6394f5d8952628
Vlad Vobiachenko <109553178+vladosi1@users.noreply.github.com>
2023-05-02 00:28:15 +0300

    +responsive mobile

--- js/main.js
+++ js/main.js
@@ -6,64 +6,29 @@ let menu = document.querySelector('.menu');
 menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
-});
+})
 
-// var swiper = new Swiper(".swiper-container", {
-// 	slidesPerView: 1,
-// 	spaceBetween: 20,
-// 	loop: true,
-// 	navigation: {
-// 	  nextEl: ".swiper-button-next",
-// 	  prevEl: ".swiper-button-prev",
-// 	},
-// 	breakpoints: {
-// 	  768: {
-// 		 slidesPerView: 2,
-// 	  },
-// 	  1024: {
-// 		 slidesPerView: 3,
-// 		 spaceBetween: 30,
-// 	  },
-// 	},
-//  });
 var swiper = new Swiper(".swiper-container", {
 	slidesPerView: 1,
 	spaceBetween: 20,
-	direction: "horizontal",
 	loop: true,
-	loopAdditionalSlides: 1,
 	navigation: {
 	  nextEl: ".swiper-button-next",
 	  prevEl: ".swiper-button-prev",
 	},
+	loop: true,                         
+    pagination: {                      
+        el: '.swiper-pagination',
+        type: 'progressbar', 
+	},
 	breakpoints: {
-	  320: {
-		 slidesPerView: 1,
-		 spaceBetween: 5,
-	  },
-	  480: {
+	  768: {
 		 slidesPerView: 2,
-		 spaceBetween: 20,
-	  },
-	  800: {
-		 slidesPerView: 3,
-		 spaceBetween: 25,
 	  },
 	  1024: {
 		 slidesPerView: 3,
-		 spaceBetween: 35,
-	  },
-	},
-	on: {
-	  reachEnd: function () {
-		 document.querySelector(".swiper-button-next").classList.toggle("hidden", true);
-	  },
-	  reachBeginning: function () {
-		 document.querySelector(".swiper-button-next").classList.toggle("hidden", false);
+		 spaceBetween: 30,
 	  },
 	},
  });
- 
-
-
 
